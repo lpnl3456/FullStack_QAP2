b86270726c01a0e921db616f813da710cd72472d
lpnl3456 <luke.peddle@keyin.com>
2024-06-07 13:11:25 -0230

    Added code to display the contacts and homepage

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
+const fs = require('fs');
 const port = 3000;
+
 
 global.DEBUG = true;
 
@@ -15,9 +17,31 @@ const server = http.createServer( async (request, response) => {
     switch(request.url) {
     case '/':
         if(DEBUG) console.log("Homepage");
+        fs.readFile(__dirname + "/views/homepage.html", function (error, html) {
+            if (error) {
+              throw error;
+            }
+
+            else{
+                if(DEBUG) console.log("Sucessfully loaded file")
+            }
+
+            response.end(html);
+          });
         break;
     case '/contacts':
         if(DEBUG) console.log('contact');
+
+        fs.readFile(__dirname + "/views/contacts.html", function (error, html) {
+            if (error) {
+              throw error;
+            }
+            else{
+                if(DEBUG) console.log("Sucessfully loaded file")
+            }
+            response.end(html);
+          });
+        
         break;
     case '/prodcuts':
         if(DEBUG) console.log('products');
