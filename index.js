6a9e1dc1b9ad27107b9545d73095e40196ce3328
lpnl3456 <luke.peddle@keyin.com>
2024-06-07 20:02:13 -0230

    Added events to descibe when page had load or if an error has occured for the homepage

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const fs = require('fs');
 const port = 3000;
 
 
+const { format, getYear } = require('date-fns');
+const { v4: uuid } = require('uuid'); //guid
+
+
+// define/extend an EventEmitter class
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+// initialize an new emitter object
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('event', async (event, level, message) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${level}\t${event}\t${message}\t${uuid()}`;
+    console.log(logItem);
+})
+
 global.DEBUG = true;
 
 const server = http.createServer( async (request, response) => {
@@ -13,22 +33,45 @@ const server = http.createServer( async (request, response) => {
       response.end();
       return;
     }
+    const fullUrl = `http://${request.headers.host}${request.url}`;
  
     switch(request.url) {
     case '/':
+        
         if(DEBUG) console.log("Homepage");
-        fs.readFile(__dirname + "/views/homepage.html", function (error, html) {
-            if (error) {
-              throw error;
-            }
-
-            else{
-                if(DEBUG) console.log("Sucessfully loaded file")
-            }
+        try{
+            fs.readFile(__dirname + "/views/homepage.html", function (error, html) {
+            try{
+ 
+            if(DEBUG) console.log("Sucessfully loaded file")
+            
             response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(html)
+            response.write(html);
             response.end();
-          });
+            
+            myEmitter.emit('event', fullUrl, 'INFO', 'Worte the homepage');
+        }
+        catch{
+            console.error(error);
+            let message = `500 - server error with internal error code of ${error.code}.`
+            myEmitter.emit('event', fullUrl, 'ERROR', message);
+        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));
+        response.end();
+
+        }
+          })
+        }
+        
+        
+        catch (error){
+            console.error(error);
+            let message = `500 - server error with internal error code of ${error.code}.`
+            myEmitter.emit('event', fullUrl, 'ERROR', message);
+        response.writeHead(500, { 'Content-Type': 'text/html' });
+        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));
+        response.end();
+        
+        }
         break;
     case '/contacts':
         if(DEBUG) console.log('contact');
