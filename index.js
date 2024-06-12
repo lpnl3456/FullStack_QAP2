725d7dd5eb0516d4d881e5ccfb8668ae2a5581dc
lpnl3456 <luke.peddle@keyin.com>
2024-06-12 10:31:48 -0230

    Added Feedback and Fixed some spelling mistakes

--- index.js
+++ index.js
@@ -14,31 +14,27 @@ const server = http.createServer( async (request, response) => {
       return;
     }
     const fullUrl = `http://${request.headers.host}${request.url}`;
-    const css =  fs.readFileSync(__dirname + "/views/Style/styles.css");
     switch(request.url) {
+    //Enter if the url is for the hompage
     case '/':
         
         if(DEBUG) console.log("Homepage");
         try{
+            //Display the homepage html file
             fs.readFile(__dirname + "/views/homepage.html", function (error, html) {
             try{
-                
- 
             response.writeHead(200, { 'Content-Type': 'text/html' });
-            if(DEBUG) console.log("Sucessfully loaded file")
-            
             response.write(html);
             if(DEBUG) console.log("Sucessfully loaded file")
             response.end();
-            
-            myEventEmitter.emit('event', fullUrl, 'INFO', 'Worte the homepage');
-            if(DEBUG) console.log("Sucessfully loaded file")
+            myEventEmitter.emit('event', fullUrl, 'INFO', 'Wrote the homepage');//Use the MyEventEmitter to write the info in a log file
         }
+
         catch{
             console.error(error);
             let message = `500 - server error with internal error code of ${error.code}.`
-            myEventEmitter.emit('event', fullUrl, 'ERROR', message);
-        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));
+            myEventEmitter.emit('event', fullUrl, 'ERROR', message);//Use the MyEventEmitter to write the error in a log file
+        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' })); //Show that an error occurred on the webpage
         response.end();
 
         }
@@ -49,13 +45,14 @@ const server = http.createServer( async (request, response) => {
         catch (error){
             console.error(error);
             let message = `500 - server error with internal error code of ${error.code}.`
-            myEventEmitter.emit('event', fullUrl, 'ERROR', message);
+            myEventEmitter.emit('event', fullUrl, 'ERROR', message);//Use the MyEventEmitter to write the error in a log file
         response.writeHead(500, { 'Content-Type': 'text/html' });
-        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));
+        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));//Show that an error occurred on the webpage
         response.end();
         
         }
         break;
+        //Enter if the url is for the contacts page
     case '/contacts':
         if(DEBUG) console.log('contact');
 
@@ -63,19 +60,19 @@ const server = http.createServer( async (request, response) => {
             fs.readFile(__dirname + "/views/contacts.html", function (error, html) {
             try{
  
-            if(DEBUG) console.log("Sucessfully loaded file")
             
+            //Write the contacts html file to the webpage
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(html);
             response.end();
-            
-            myEventEmitter.emit('event', fullUrl, 'INFO', 'Worte the contact page');
+            if(DEBUG) console.log("Sucessfully loaded file");
+            myEventEmitter.emit('event', fullUrl, 'INFO', 'Wrote the contact page');//Use the MyEventEmitter to write the info in a log file
         }
         catch{
             console.error(error);
             let message = `500 - server error with internal error code of ${error.code}.`
-            myEventEmitter.emit('event', fullUrl, 'ERROR', message);
-        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));
+            myEventEmitter.emit('event', fullUrl, 'ERROR', message); //Use the MyEventEmitter to write the error in a log file
+        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));//Show that an error occurred on the webpage
         response.end();
 
         }
@@ -86,34 +83,33 @@ const server = http.createServer( async (request, response) => {
         catch (error){
             console.error(error);
             let message = `500 - server error with internal error code of ${error.code}.`
-            myEventEmitter.emit('event', fullUrl, 'ERROR', message);
+            myEventEmitter.emit('event', fullUrl, 'ERROR', message);//Use the MyEventEmitter to write the error in a log file
         response.writeHead(500, { 'Content-Type': 'text/html' });
-        response.write(JSON.stringify({ error: 'An error occurred while loading the contact page' }));
+        response.write(JSON.stringify({ error: 'An error occurred while loading the contact page' }));//Show that an error occurred on the webpage
         response.end();
         
         }
         
         break;
-
+    //Enter if the url is for the products page
     case '/products':
         if(DEBUG) console.log('products');
         try{
+
             fs.readFile(__dirname + "/views/products.html", function (error, html) {
             try{
- 
-            if(DEBUG) console.log("Sucessfully loaded file")
-            
+            //Write the products html file to the webpage
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(html);
             response.end();
-            
-            myEventEmitter.emit('event', fullUrl, 'INFO', 'Wrote the product page');
+            if(DEBUG) console.log("Sucessfully loaded file");
+            myEventEmitter.emit('event', fullUrl, 'INFO', 'Wrote the product page'); //Use the MyEventEmitter to write the info in a log file
         }
         catch{
             console.error(error);
             let message = `500 - server error with internal error code of ${error.code}.`
-            myEventEmitter.emit('event', fullUrl, 'ERROR', message);
-        response.write(JSON.stringify({ error: 'An error occurred while loading the product page' }));
+            myEventEmitter.emit('event', fullUrl, 'ERROR', message);//Use the MyEventEmitter to write the error in a log file
+        response.write(JSON.stringify({ error: 'An error occurred while loading the product page' })); //Show that an error occurred on the webpage
         response.end();
 
         }
@@ -124,34 +120,36 @@ const server = http.createServer( async (request, response) => {
         catch (error){
             console.error(error);
             let message = `500 - server error with internal error code of ${error.code}.`
-            myEventEmitter.emit('event', fullUrl, 'ERROR', message);
+            myEventEmitter.emit('event', fullUrl, 'ERROR', message); //Use the MyEventEmitter to write the error in a log file
         response.writeHead(500, { 'Content-Type': 'text/html' });
-        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));
+        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));//Show that an error occurred on the webpage
         response.end();
         
         }
 
         break;
 
+    //Enter if the url is for the subscribe page
     case '/subscribe':
         if(DEBUG) console.log('subscribe');
         try{
+            
             fs.readFile(__dirname + "/views/subscribe.html", function (error, html) {
             try{
  
-            if(DEBUG) console.log("Sucessfully loaded file")
             
+            //Write the subscribe html file to the webpage
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(html);
             response.end();
-            
-            myEventEmitter.emit('event', fullUrl, 'INFO', 'Wrote the subscribe page');
+            if(DEBUG) console.log("Sucessfully loaded file");
+            myEventEmitter.emit('event', fullUrl, 'INFO', 'Wrote the subscribe page');//Use the MyEventEmitter to write the info in a log file
         }
         catch{
             console.error(error);
             let message = `500 - server error with internal error code of ${error.code}.`
-            myEventEmitter.emit('event', fullUrl, 'ERROR', message);
-        response.write(JSON.stringify({ error: 'An error occurred while loading the subscribe page' }));
+            myEventEmitter.emit('event', fullUrl, 'ERROR', message);//Use the MyEventEmitter to write the error in a log file
+        response.write(JSON.stringify({ error: 'An error occurred while loading the subscribe page' }));//Show that an error occurred on the webpage
         response.end();
 
         }
@@ -162,32 +160,34 @@ const server = http.createServer( async (request, response) => {
         catch (error){
             console.error(error);
             let message = `500 - server error with internal error code of ${error.code}.`
-            myEventEmitter.emit('event', fullUrl, 'ERROR', message);
+            myEventEmitter.emit('event', fullUrl, 'ERROR', message);//Use the MyEventEmitter to write the error in a log file
         response.writeHead(500, { 'Content-Type': 'text/html' });
-        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));
+        response.write(JSON.stringify({ error: 'An error occurred while loading the homepage' }));//Show that an error occurred on the webpage
         response.end();
         
         }
         break;
+    
+    //Enter if the url is for the about page
     case '/about':
         if(DEBUG) console.log('about');
         try{
             fs.readFile(__dirname + "/views/about.html", function (error, html) {
             try{
  
-            if(DEBUG) console.log("Sucessfully loaded file")
             
+            //Write the about html file to the webpage
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(html);
             response.end();
-            
-            myEventEmitter.emit('event', fullUrl, 'INFO', 'Wrote the about page');
+            if(DEBUG) console.log("Sucessfully loaded file");
+            myEventEmitter.emit('event', fullUrl, 'INFO', 'Wrote the about page');//Use the MyEventEmitter to write the info in a log file
         }
         catch{
             console.error(error);
             let message = `500 - server error with internal error code of ${error.code}.`
-            myEventEmitter.emit('event', fullUrl, 'ERROR', message);
-        response.write(JSON.stringify({ error: 'An error occurred while loading the about page' }));
+            myEventEmitter.emit('event', fullUrl, 'ERROR', message);//Use the MyEventEmitter to write the error in a log file
+        response.write(JSON.stringify({ error: 'An error occurred while loading the about page' }));//Show that an error occurred on the webpage
         response.end();
 
         }
@@ -198,16 +198,16 @@ const server = http.createServer( async (request, response) => {
         catch (error){
             console.error(error);
             let message = `500 - server error with internal error code of ${error.code}.`
-            myEventEmitter.emit('event', fullUrl, 'ERROR', message);
+            myEventEmitter.emit('event', fullUrl, 'ERROR', message);//Use the MyEventEmitter to write the error in a log file
         response.writeHead(500, { 'Content-Type': 'text/html' });
-        response.write(JSON.stringify({ error: 'An error occurred while loading the about' }));
+        response.write(JSON.stringify({ error: 'An error occurred while loading the about' }));//Show that an error occurred on the webpage
         response.end();
         
         }
         break;
-    
 }
 
+//Enter to read and display the CSS files for the html files
 if (request.url.match(".css$")) {
     const cssPath = (__dirname + "/views/Style/styles.css");
     const fileStream = fs.createReadStream(cssPath, "UTF-8");
